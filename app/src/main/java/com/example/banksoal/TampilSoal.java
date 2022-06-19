5f33ef677b5d94c3c1604896adf4a543fcfe8f8d
fitri alfiana <fitrialfiana62@gmail.com>
2022-06-20 00:18:42 +0700

    update revisi

---
+++ app/src/main/java/com/example/banksoal/TampilSoal.java
@@ -0,0 +1,14 @@
+package com.example.banksoal;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class TampilSoal extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_tampil_soal);
+    }
+}
